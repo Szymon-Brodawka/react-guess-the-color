dde54b63a481baec1011970dba66d21b994eb963
Szymon Brodawka <szymbrod@gmail.com>
2025-02-26 16:35:20 +0100

    Add references to the color answers and add a function updating all of their styles after clicking one of them

--- src/components/ColorAnswer.jsx
+++ src/components/ColorAnswer.jsx
@@ -1,15 +1,21 @@
 import { useContext } from "react";
-import { ColorsContext, RightColorContext } from "../Contexts.js";
+import { ColorAnswersContext, RightColorContext } from "../Contexts.js";
 
-export default function ColorAnswer( { color }){
-    const { updateHexColors } = useContext(ColorsContext);
+export default function ColorAnswer( { color, index, colorRefs }){
+    const colorRefs = useContext(ColorAnswersContext);
     const rightColor = useContext(RightColorContext);
+
+    const update = () => {
+        colorRefs.current.forEach((colorRef) => {
+            colorRef.style.backgroundColor = colorRef.textContent === rightColor ? "green" : "red"
+        });
+    }
 
     return(
         <div
-            onClick={updateHexColors}
             className="bg-neutral-200 text-neutral-900 cursor-pointer px-2 py-2 rounded-3xl hover:bg-neutral-900 transition-colors duration-150 hover:text-neutral-200"
-            style={{ backgroundColor: color === rightColor ? "green" : "red" }}
+            ref={(element) => colorRefs.current[index] = element}
+            onClick={update}
         >
             {color}
         </div>
--- src/components/ColorsRow.jsx
+++ src/components/ColorsRow.jsx
@@ -1,14 +1,16 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import ColorAnswer from "./ColorAnswer";
 import { ColorsContext } from "../Contexts.js";
 
 export default function ColorsRow() {
+    const colorRefs = useRef([]);
     const { hexColors } = useContext(ColorsContext);
-    
+
     return(
         <div className="flex flex-row gap-x-5">
             {
-                hexColors.map((hexColor) => <ColorAnswer color={hexColor} key={hexColor}></ColorAnswer>)
+                hexColors.map((hexColor, i) => 
+                    <ColorAnswer color={hexColor} key={hexColor} index={i} colorRefs={colorRefs}></ColorAnswer>)
             }
         </div>
     )
