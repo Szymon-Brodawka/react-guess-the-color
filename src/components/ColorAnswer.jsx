b9ead2f3e7d71c0a94d99a97478fd9a87033cafe
Szymon Brodawka <szymbrod@gmail.com>
2025-02-26 16:37:43 +0100

    Delete importing non existing context from the ColorAnswer component

--- src/components/ColorAnswer.jsx
+++ src/components/ColorAnswer.jsx
@@ -1,8 +1,7 @@
 import { useContext } from "react";
-import { ColorAnswersContext, RightColorContext } from "../Contexts.js";
+import { RightColorContext } from "../Contexts.js";
 
 export default function ColorAnswer( { color, index, colorRefs }){
-    const colorRefs = useContext(ColorAnswersContext);
     const rightColor = useContext(RightColorContext);
 
     const update = () => {
@@ -12,12 +11,12 @@ export default function ColorAnswer( { color, index, colorRefs }){
     }
 
     return(
-        <div
+        <button
             className="bg-neutral-200 text-neutral-900 cursor-pointer px-2 py-2 rounded-3xl hover:bg-neutral-900 transition-colors duration-150 hover:text-neutral-200"
             ref={(element) => colorRefs.current[index] = element}
             onClick={update}
         >
             {color}
-        </div>
+        </button>
     )
 }
