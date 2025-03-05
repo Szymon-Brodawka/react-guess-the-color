7e2e67024600cb57295526f64517abd6b797d16f
Szymon Brodawka <szymbrod@gmail.com>
2025-03-05 17:26:19 +0100

    Remove unused code from custom useUpdateColors hook

--- src/hooks/useUpdateColors.js
+++ src/hooks/useUpdateColors.js
@@ -2,14 +2,14 @@ import { useCallback, useEffect, useRef } from "react";
 import useTimeout from "./useTimeout";
 
 export default function useUpdateColors(colorRefs, rightColor, updateHexColors) {
-    const { clear, reset } = useTimeout(updateHexColors, 1000);
+    const DELAY = 1000;
+    const { reset } = useTimeout(updateHexColors, DELAY);
     const updateAnwersColor = () => {
         colorRefs.current.forEach((colorRef) => {
             colorRef.style.backgroundColor = colorRef.textContent === rightColor ? "green" : "red"
         });
     }
     
-    clear();
     const update = useCallback(() => {
         reset();
         updateAnwersColor();
